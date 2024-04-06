3971ed1834537e94bcd12c3035fe3f80af10b6a7
Debashis B <debashis.baidya@kiwigrid.com>
2024-04-06 13:49:17 +0200

    Kafka message sending implemented and tested.

---
+++ src/main/java/com/learning/kafka/apache/ApacheApplication.java
@@ -0,0 +1,13 @@
+package com.learning.kafka.apache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApacheApplication.class, args);
+	}
+
+}
